afb3587e7a709534968c24bf0ddb8be264744fd3
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2017-11-27 15:13:36 +1100

    Added class VIPMember

--- membership-inheritance/Program.cs
+++ membership-inheritance/Program.cs
@@ -65,3 +65,17 @@ class NormalMember : Member
     }
 
 }
+
+class VIPMember : Member
+{
+    public VIPMember(string name, int memberID, int memberSince) 
+        : base (name, memberID, memberSince)
+    {
+        Console.WriteLine("Child Constructor with 3 parameters");
+    }
+
+    public void CalculateAnnualFee()
+    {
+        annualFee = 1200;
+    }
+}
