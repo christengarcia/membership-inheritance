9fa94994ae7a897250345052547c99c4ee730363
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2017-11-27 14:55:08 +1100

    Added two constructors in the Member class

--- membership-inheritance/Program.cs
+++ membership-inheritance/Program.cs
@@ -10,7 +10,7 @@ namespace membership_inheritance
     {
         static void Main(string[] args)
         {
-            
+
         }
     }
 }
@@ -21,12 +21,26 @@ class Member
     private string name;
     private int memberID;
     private int memberSince;
-}
 
-public override string ToString()
-{
-    return "\name: " + name + "\nMember ID: " + memberID
-                              + "\nMember Since: " + memberSince
-                              + "\nTotal Annual Fee: " + annualFee; 
-}
 
+    public override string ToString()
+    {
+        return "\name: " + name + "\nMember ID: " + memberID
+                                  + "\nMember Since: " + memberSince
+                                  + "\nTotal Annual Fee: " + annualFee;
+    }
+
+    public Member()
+    {
+        Console.WriteLine("Parent Constructor with no parameter");
+    }
+
+    public Member(string pName, int pMemberID, int pMemberSince)
+    {
+        Console.WriteLine("Parent Constructor with 3 parameters");
+
+        name = pName;
+        pMemberID = pMemberID;
+        pMemberSince = pMemberSince;
+    }
+}
